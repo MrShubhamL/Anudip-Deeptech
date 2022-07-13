06a6f49f7b6e6521a7add7d7958f7e6a8f6e57d6
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-13 17:49:43 +0530

    Program to print the user name
    Java Programming

---
+++ Day-3/UserName.java
@@ -0,0 +1,10 @@
+// Program for printing the Name of user.
+// Day - 3rd
+// Date - 13/07/2022
+// **********************************************
+
+class UserName{
+    public static void main(String m[]){
+      System.out.println("Shubham");
+   }
+}
