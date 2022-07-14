edf148b721e0a36c29e7c1793935be10c1b1cfb3
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-14 16:51:45 +0530

    (Type Casting) Winding

---
+++ Day-4/Winding/Winding.java
@@ -0,0 +1,18 @@
+class Winding
+{
+public static void main(String[] args) 
+{
+		
+	int c = 67;
+	int d = 33;
+	
+	double a = c;
+	double b = d;
+
+	System.out.println("Integer into Double : " + a);
+	System.out.println("Integer into Double : " + b);	
+	
+
+}
+
+}
