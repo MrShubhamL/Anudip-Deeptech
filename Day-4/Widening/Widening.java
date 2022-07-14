17a8e0ee278a221ca67a745639f1b2b1546bae4c
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-14 17:17:27 +0530

    Add files via upload

---
+++ Day-4/Widening/Widening.java
@@ -0,0 +1,18 @@
+class Winding
+{
+public static void main(String[] args) 
+{
+		
+	int c = 67;
+	int d = 33;
+	
+	double a = c;
+	double b = d;
+
+	System.out.println("(67) Integer into Double : " + a);
+	System.out.println("(33) Integer into Double : " + b);	
+	
+
+}
+
+}
