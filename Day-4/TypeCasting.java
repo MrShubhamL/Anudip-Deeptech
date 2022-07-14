ec8b4cdb28cf3479454842acb2aed09ec2aaabd7
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-14 15:27:27 +0530

    Type Casting
    Day-4

---
+++ Day-4/TypeCasting.java
@@ -0,0 +1,17 @@
+class JavaProgramming 
+{
+public static void main(String[] args) 
+{
+		
+	char ch = 'A';
+	byte value = (byte) ch;
+
+	char c = (char)value;
+
+	System.out.println("char : " + c);
+	System.out.println("byte: " + value);
+	
+
+}
+
+}
