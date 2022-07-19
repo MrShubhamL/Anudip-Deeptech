6c703dac84633f53e116b467c3e0b180c996a845
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-19 15:53:46 +0530

    Pattern In java

---
+++ Day-6/Patterns.java
@@ -0,0 +1,16 @@
+class Patterns {
+	public static void main(String[] args) {
+		for(int i=0; i<6; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=6; i>=0; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
