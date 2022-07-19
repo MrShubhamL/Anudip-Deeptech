bdfa13b3b123d4558890a7a09a262981c2856f25
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-19 17:49:18 +0530

    Printing the value from 11 to 3

---
+++ Day-6/ForLoop2.java
@@ -0,0 +1,10 @@
+ class ForLoop {
+	public static void main(String[] args) {
+		System.out.println("Printing the value from 10 to 6");
+		for(int i=11; i>3; i--) {
+			System.out.println("The value of i: " + i);
+		}
+		
+		
+	}
+}
