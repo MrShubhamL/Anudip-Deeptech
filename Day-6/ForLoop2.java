a457f80818e64bc2abda3b5a49e642effa6d08fe
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-19 17:54:13 +0530

    Printing the number from 10 to 6

--- Day-6/ForLoop2.java
+++ Day-6/ForLoop2.java
@@ -1,6 +1,6 @@
  class ForLoop {
 	public static void main(String[] args) {
-		System.out.println("Printing the value from 10 to 6");
+		System.out.println("Printing the value from 10 to 6.");
 		for(int i=10; i>6; i--) {
 			System.out.println("The value of i: " + i);
 		}
