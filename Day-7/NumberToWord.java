34dd20f08e72828d15935bbc542f48e687790a4f
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-20 16:25:46 +0530

    Number to Word

---
+++ Day-7/NumberToWord.java
@@ -0,0 +1,11 @@
+public class NumberToWord {
+
+	public static void main(String[] args) {
+		int num = 68;
+		char c;
+		c = (char) num;
+		System.out.println("The number "+num+" is converted into " + c);
+
+	}
+
+}
