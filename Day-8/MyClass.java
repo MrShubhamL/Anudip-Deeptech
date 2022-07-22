4304a4d731df9d4620dcf4c0cbd44639dd3c3f25
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-22 16:25:11 +0530

    Classes & Objects

---
+++ Day-8/MyClass.java
@@ -0,0 +1,14 @@
+class MyClass{
+int x = 6;
+int y = 7;
+int z = 8;
+public static void main(String args[]){
+ MyClass s = new MyClass();
+   System.out.println("Value of x: " + s.x);
+   System.out.println("Value of y: " + s.y);
+   System.out.println("Value of z: " + s.z);	
+
+}
+
+
+}
