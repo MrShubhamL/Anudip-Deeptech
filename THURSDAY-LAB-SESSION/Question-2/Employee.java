d69b38056ba16fd8a77dedda1e931e96bdb9f597
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-28 15:51:31 +0530

    Employee

---
+++ THURSDAY-LAB-SESSION/Question-2/Employee.java
@@ -0,0 +1,35 @@
+
+class Employee {
+	private String name;
+	private short yearOfJoining;
+	private double salary;
+	private String address;
+	
+	public Employee(String name, short yearOfJoining, double salary, String address) {
+		this.name = name;
+		this.yearOfJoining = yearOfJoining;
+		this.salary = salary;
+		this.address = address;
+	}
+	
+	
+	public String toString() {
+		return name + "\t\t" + yearOfJoining + "\t\t   " + salary + "\t" + address;
+	}
+
+
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("Rohit S Patil", (short)2021, 80000, "Kolhapur Maharashtra");
+		Employee e2 = new Employee("Karan A Jadhav", (short)2018, 75000, "Mumbai Maharashtra");
+		Employee e3 = new Employee("Omkar P Koli", (short)2015, 65000, "Sangali Maharashtra");
+		
+		System.out.println("Employee Name \t Year of Joining \t Total Salary \t Address");
+		System.out.println(e1.toString());
+		System.out.println(e2.toString());
+		System.out.println(e3.toString());
+	}
+	
+	
+
+}
