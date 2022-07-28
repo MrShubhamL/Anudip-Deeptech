79f856f0c15d05c474d16ede26940639082070e5
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-28 16:23:07 +0530

    Book Store

---
+++ THURSDAY-LAB-SESSION/Question-3/Book.java
@@ -0,0 +1,65 @@
+ class Book {
+	
+	private String bookName;
+	private String isbnNumber;
+	private String authorName;
+	private String publisher;
+	
+	public Book(String bookName, String isbnNumber, String authorName, String publisher) {
+		this.bookName = bookName;
+		this.isbnNumber = isbnNumber;
+		this.authorName = authorName;
+		this.publisher = publisher;
+	}
+	
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getIsbnNumber() {
+		return isbnNumber;
+	}
+
+	public void setIsbnNumber(String isbnNumber) {
+		this.isbnNumber = isbnNumber;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public String getBookInfo() {
+		String description = "Book Name: " + getBookName() + "\n" +
+								"ISBN Number: " + getIsbnNumber() + "\n" +
+								"Author Name: " + getAuthorName() + "\n" +
+								"Publisher: " + getPublisher();
+		return description;
+	}
+	
+	public static void main(String[] args) {
+		Book b = new Book("Information Technology", "SN00235", "Stanley Wright", "Express Publishing");
+		System.out.println(b.getBookInfo());
+		
+		Book test[] = new Book[30];
+
+	}
+	
+
+}
