5b64f32a518e3d317dc743a54af9de208b5ef31d
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-08-16 15:32:54 +0530

    Autoboxing & Unboxing

--- WrapperClasses/WrapperClass.java
+++ WrapperClasses/WrapperClass.java
@@ -1,6 +1,6 @@
 
 //Autoboxing
-public class WrapperClasses {
+class WrapperClasses {
 	public static void main(String[] args) {
 		int a = 89;
 		Integer integer = Integer.valueOf(a);
@@ -11,6 +11,21 @@ public class WrapperClasses {
 		Boolean b = Boolean.valueOf(f);
 		System.out.println("Autoboxing (boolean to Boolean): " + f + " " + b);
 		Unboxing.example2();
+		
+		char c = 'A';
+		Character ch = Character.valueOf(c);
+		System.out.println("Autoboxing (char to Character): " + c + " " + ch);
+		Unboxing.example3();
+		
+		byte bt = 127;
+		Byte byt = Byte.valueOf(bt);
+		System.out.println("Autoboxing (byte to Byte): " + bt + " " + byt);
+		Unboxing.example4();
+		
+		short s = (short) 34564;
+		Short sh = Short.valueOf(s);
+		System.out.println("Autoboxing (short to Short): " + s + " " + sh);
+		Unboxing.example5();
 	}
 
 }
@@ -28,5 +43,23 @@ class Unboxing{
 		Boolean b = new Boolean(true);
 		boolean f = b.booleanValue();
 		System.out.println("Unboxing (Boolean to boolean) : " + b + " " + f);
+	}
+	
+	static void example3() {
+		Character c = new Character('D');
+		char ch = c.charValue();
+		System.out.println("Unboxing (Character to char) : " + c + " " + ch);
+	}
+	
+	static void example4() {
+		Byte bt = new Byte((byte) -128);
+		byte byt = bt.byteValue();
+		System.out.println("Unboxing (Byte to byte) : " + bt + " " + byt);
+	}
+	
+	static void example5() {
+		Short st = new Short((short) 678);
+		short s = st.shortValue();
+		System.out.println("Unboxing (Short to short) : " + st + " " + s);
 	}
 }
